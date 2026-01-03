474e638d26d68f410078fee7ce7e47b0330403dd
Shravanthi20 <satyanar061@gmail.com>
2026-01-03 23:54:37 +0530

    Added testing framework and CI/CD pipeline

--- backend/app.js
+++ backend/app.js
@@ -197,6 +197,4 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-});
+module.exports = app;
